9b27a0c70d8aa482e068ddf130c1061d9517614a
Chloe Bradley <ccbradley77@gmail.com>
2015-06-22 20:47:07 -0600

    events index background image is set

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,44 +22,13 @@ class EventsController < ApplicationController
     else
       @events = []
     end
+
     @hash = Gmaps4rails.build_markers(@events) do |event, marker|
-    marker.lat event["latitude"]
-    marker.lng event["longitude"]
-  end
-  end
-
-  # GET /events/1
-  # GET /events/1.json
-  def show
+      marker.lat event["latitude"]
+      marker.lng event["longitude"]
+    end
   end
 
-  # # GET /events/new
-  # def new
-  #   @event = Event.new
-  # end
-
-  # GET /events/1/edit
-  def edit
-  end
-
-  # POST /events
-  # POST /events.json
-  # def create
-  #   @event = Event.new(event_params)
-  #
-  #   respond_to do |format|
-  #     if @event.save
-  #       format.html { redirect_to @event, notice: 'Event was successfully created.' }
-  #       format.json { render :show, status: :created, location: @event }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @event.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -72,8 +41,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
@@ -83,13 +50,7 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
-
-    # # Never trust parameters from the scary internet, only allow the white list through.
-    # def event_params
-    #   params.require(:events).permit(:venue_address, :latitude, :longitude)
-    # end
 end
